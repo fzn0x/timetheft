4cb91f926882f748511f0cb3606ee79bbf447579
fncolon <fncolon@pm.me>
2021-11-26 01:35:48 +0700

    docs: add isFinish example

--- examples/mjs.js
+++ examples/mjs.js
@@ -1,9 +1,16 @@
 import theft from "../src/timetheft.mjs";
 
-theft("60s", ({ years, months, weeks, days, hours, minutes, seconds }) => {
-  // do whatever with years, months, weeks, days, hours, minutes, seconds
-  // e.g just console.log it :smile:
-  console.log(
-    `Elapsed years: ${years}, months: ${months}, weeks: ${weeks}, days: ${days}, hours: ${hours}, minutes: ${minutes}, seconds: ${seconds}`
-  );
-});
+theft(
+  "60s",
+  ({ years, months, weeks, days, hours, minutes, seconds, isFinish }) => {
+    // do whatever with years, months, weeks, days, hours, minutes, seconds
+    // e.g just console.log it :smile:
+    console.log(
+      `Elapsed years: ${years}, months: ${months}, weeks: ${weeks}, days: ${days}, hours: ${hours}, minutes: ${minutes}, seconds: ${seconds}`
+    );
+
+    if (isFinish) {
+      console.log("Done!");
+    }
+  }
+);
